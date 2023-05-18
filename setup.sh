a8b1955efc1e59c90089e3b81cb26295458d3a61
BrianSalk <brian.salkas@gmail.com>
2023-05-18 14:03:34 -0500

    add comments and check for cargo in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #! /bin/bash
+## this script ensures that the use has cargo installed, 
+# if the user does not have cargo installed, then dispaly error and return 1
+# else attempt to create script /usr/local/bin/wordle
+# if file already exists prompt user until user enters file that does not
+# exist in that directory
+# if the event that the user does not have /usr/local/bin directory, 
+# this script will not work ##
+if command -v cargo > /dev/null 2>&1
+then
+	echo >&2 'setting up wordle...'
+else
+	echo >&2 'please install cargo in order to use this app'
+	exit 1
+
+fi
 cargo build --release
 CURR_PATH="$(pwd)"
 COMMAND='wordle'
